c17c759779c410560f126ea29925483a601c410c
Stas Fomin <stas-fomin@yandex.ru>
2020-04-18 17:46:35 +0300

    workaround still not worked

--- nuitka-develop-0.6.8rc5-numpy-plugin-problems/!build-windows-anaconda-32-3.7-workaround.bat
+++ nuitka-develop-0.6.8rc5-numpy-plugin-problems/!build-windows-anaconda-32-3.7-workaround.bat
@@ -2,4 +2,4 @@
 set PATH=C:\anaconda32-3.7;C:\anaconda32-3.7\Scripts;%PATH%;
 set NUITKA_CLCACHE_BINARY=C:\anaconda32-3.7\Scripts\clcache.exe
 rem C:\anaconda32-3.7\python -m nuitka --version
-python -m nuitka --standalone demo.py --verbose --jobs=4   --plugin-enable=numpy  --output-dir=w32-wtf
+python -m nuitka --standalone demo.py --verbose --jobs=4   --plugin-enable=numpy  --include-scipy=False --include-matplotlib=False --output-dir=w32-wtf
