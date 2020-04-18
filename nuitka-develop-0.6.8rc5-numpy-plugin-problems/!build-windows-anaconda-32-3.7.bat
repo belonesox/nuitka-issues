a14907af6e4f4c16f1f9e77c13fd553d5a6aa601
Stas Fomin <stas-fomin@yandex.ru>
2020-04-18 15:16:12 +0300

    numpy plugins problem

---
+++ nuitka-develop-0.6.8rc5-numpy-plugin-problems/!build-windows-anaconda-32-3.7.bat
@@ -0,0 +1,5 @@
+﻿rem
+set PATH=C:\anaconda32-3.7;C:\anaconda32-3.7\Scripts;%PATH%;
+set NUITKA_CLCACHE_BINARY=C:\anaconda32-3.7\Scripts\clcache.exe
+rem C:\anaconda32-3.7\python -m nuitka --version
+python -m nuitka --recurse-none --standalone demo.py --verbose --jobs=4   --plugin-enable=numpy  --output-dir=w32-wtf
